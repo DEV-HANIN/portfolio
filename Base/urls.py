d52f4856551cd3bc8a50afe9258b224bab38054d
DIV-HANIN <hadeel20tech@gmail.com>
2025-04-15 05:01:30 +0300

    Add Procfile, requirements, and update settings

--- Base/urls.py
+++ Base/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact, name='home'),  # ← أضف name='home' هنا
-]
+    path('', views.contact, name='home'), ] 
