b9fede0bee56e70ad4b76bb3328b25ca5ecaf9c4
DIV-HANIN <hadeel20tech@gmail.com>
2025-07-23 10:55:11 +0300

    Fix static file duplicates and cleanup

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'Base/static')
-]
+# إلغاء STATICFILES_DIRS نهائيًا
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'Base/static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
