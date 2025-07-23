94fe23a3c92e5e5a5629e97b2f9246a63d68811e
DIV-HANIN <hadeel20tech@gmail.com>
2025-07-23 09:00:13 +0300

    Fix static file duplication

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'Base/static')
 ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
